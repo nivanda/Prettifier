15478a6f7a0572ac37dbe9b53711226f42452a45
nivanda <partlas.martin@gmail.com>
2026-03-27 12:25:53 +0200

    Initialized the prgram in Prettifier class and added PrettifierMain function to handle all the rest of the the program

--- src/main/java/net/nivanda/prettifier/Prettifier.java
+++ src/main/java/net/nivanda/prettifier/Prettifier.java
@@ -1,11 +1,17 @@
 package net.nivanda.prettifier;
 
 import net.nivanda.prettifier.util.FileIO;
+import net.nivanda.prettifier.util.AirportCodeManager;
 
 import java.util.List;
+import java.util.Map;
 
 public class Prettifier {
     public static void main(String[] args) {
+        String content;
+        List<String[]> airportLookup;
+        Map<String, Integer> orderMap;
+
         if (args.length == 0) {
             System.out.println("itinerary usage:\n$ java Prettifier.java ./input.txt ./output.txt ./airport-lookup.csv");
         } else if (args[0].equals("-h") && args.length == 1) {
@@ -13,10 +19,18 @@ public class Prettifier {
         } else if (args.length < 3) {
             System.out.println("itinerary usage:\n$ java Prettifier.java ./input.txt ./output.txt ./airport-lookup.csv");
         } else {
-            List<String[]> testCSV = FileIO.readCsv(args[2]);
-            for (String[] line : testCSV) {
-                System.out.println(line[5]);
-            }
+            content = FileIO.readTxt(args[0]);
+            if (content == null) return;
+            airportLookup = FileIO.readCsv(args[2]);
+            if (airportLookup == null) return;
+            if (FileIO.isCSVCorrupted(airportLookup)) return;
+            orderMap = FileIO.getCSVColumnOrderMap(airportLookup.getFirst());
+            airportLookup.removeFirst();
+            PrettifierMain(content, airportLookup, orderMap);
         }
+    }
+
+    public static void PrettifierMain(String content, List<String[]> airportLookup, Map<String, Integer> orderMap) {
+
     }
 }
