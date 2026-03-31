23667ffb1ef86d1358be404f117401c5d0c011ea
nivanda <partlas.martin@gmail.com>
2026-03-31 21:03:00 +0300

    Added BonusContent bool variable to switch bonus content off and on

--- src/main/java/net/nivanda/prettifier/Prettifier.java
+++ src/main/java/net/nivanda/prettifier/Prettifier.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Prettifier {
+
+    final boolean BonusContent = false;
+
     public static void main(String[] args) {
         String content;
         List<String[]> airportLookup;
