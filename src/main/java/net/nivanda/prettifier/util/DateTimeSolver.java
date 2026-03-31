c909901688867e32631d8ea34a197d3e236474c8
nivanda <partlas.martin@gmail.com>
2026-03-31 19:09:32 +0300

    Added DateTimeSolver class to handle ISO 8601 appearances in the input text

---
+++ src/main/java/net/nivanda/prettifier/util/DateTimeSolver.java
@@ -0,0 +1,41 @@
+package net.nivanda.prettifier.util;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.ZonedDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateTimeSolver {
+
+    public static String solveDateTime(String content) {
+        Pattern pattern = Pattern.compile("(D|T12|T24)\\(([^)]+)\\)");
+        Matcher matcher = pattern.matcher(content);
+        StringBuilder result = new StringBuilder();
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder().appendOffset("+HH:MM", "+00:00").toFormatter();
+        while(matcher.find()) {
+            String format = matcher.group(1);
+            String date = matcher.group(2);
+            try {
+                ZonedDateTime zone = ZonedDateTime.parse(date);
+                String time;
+                switch (format) {
+                    case "D":
+                        time = zone.format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+                    case "T12":
+                        time = zone.format(DateTimeFormatter.ofPattern("hh:mma")) + " (" + zone.format(formatter) + ")";
+                    case "T24":
+                        time = zone.format(DateTimeFormatter.ofPattern("HH:mm")) + " (" + zone.format(formatter) + ")";
+                    default:
+                        time = matcher.group(0);
+                }
+                matcher.appendReplacement(result, Matcher.quoteReplacement(time));
+            } catch (DateTimeParseException e) {
+                matcher.appendReplacement(result, Matcher.quoteReplacement(matcher.group(0)));
+            }
+        }
+        matcher.appendTail(result);
+        return result.toString();
+    }
+}
