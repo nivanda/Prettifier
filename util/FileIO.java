ae18ab24c3e84aa9947ec2ce418e1e14675c588e
nivanda <partlas.martin@gmail.com>
2026-04-17 04:29:22 +0300

    Removed comment madness and wrote 3rd iteration of readCsv function

--- util/FileIO.java
+++ util/FileIO.java
@@ -1,23 +1,11 @@
 package util;
 
-// Imports for all the needed functions and shit.
-// Not sure if it's okay to import with *.
-// Maybe need to change back to single imports.
-// If reviewers burn my ass I will know why.
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
 
-// As you will see my naming schemes are HORRIBLE.
-// That's why I am here writing comments at 1AM,
-// so you (reviewer) can understand wtf the code I wrote does.
 public class FileIO {
-
-    // Reads txt files, duh.
-    // It only reads .txt files and nothing else...and is used only once.
-    // Note to self: If the input file gets too big,
-    // this function may lag the shit of your computer,
-    // since it has to load the whole file to a SINGLE string.
+    // Reads the input file into a singular string, might freeze up with huge files.
     public static String readTxt(String path) {
         Path filepath = Paths.get(path);
         try {
@@ -28,10 +16,7 @@ public class FileIO {
         }
     }
 
-    // Same as previous.
-    // But it can also be used for writing a test input file.
-    // Note to self: Have not tested writing huge strings,
-    // may crash the shitbox laptop if not careful (kernel panic).
+    // Writes the modified string into the output file.
     public static void writeTxt(String path, String content) {
         try {
             FileWriter fileWriter = new FileWriter(path);
@@ -43,67 +28,42 @@ public class FileIO {
         }
     }
 
-    // Oh boy, this was a fucky wucky thing to write.
-    // Originally this was just few lines of code.
-    // But now it's this piece of spaghetti code.
-    // Since I realized I can't run non-compiled code,
-    // while also using external packages.
-    // So bye bye OpenCSV and welcome yandere dev level code.
-    // Also do not question my questionable design choices
-    // of using an Object just for returning two values at once.
-    // I do not know why I did that either.
-    public static AirportLookup readCsv(String path) {
-        // WHY DID I MAKE THIS LIKE THAT?! (╯°□°)╯︵ ┻━┻
+    // REMOVE BEFORE PUSHING INTO REVIEW PHASE!!!
+    public static AirportLookup OldreadCsv(String path) {
         String LEGAL_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNM" +
                                     "qwertyuiopasdfghjklzxcvbnm" +
                                     "1234567890" +
                                     "!@#$%^&*()-_=+\\|[]{};':\",.<>/?`~ ";
         try {
-            // Preps for the horrible for loop.
-            // csv and map variables are the outputs.
-            // Note to self, if the airport-loopup.csv file is 500k lines instead of 4k,
-            // prepare for kernel panic (aka the shitbox laptop is not surving).
             List<String[]> csv = new ArrayList<>();
             List<String> lines = Files.readAllLines(Paths.get(path));
             Map<String, Integer> map = getCSVColumnOrderMap(lines.getFirst().split(","));
             lines.removeFirst();
-            // Iterates over EVERY LINE IN THE DAMN FILE.
             for (String line : lines) {
-                // Wow, this naming scheme rocks.....
                 String tempLine = line;
                 boolean skipComa = false;
                 String[] splitLine = new String[6];
                 int wordIndex = 0;
                 int removedChar = 0;
-                // Iterates over EVERY LETTER in the line...
-                // Have fun debugging this....
                 for (int i = 0; i < line.length(); i++) {
-                    // Everything that isn't in english alphabet will get flagged as "malformed".
-                    // That also means that the French habibis and Chinese comrades and can't use this tool.
                     if (LEGAL_CHARACTERS.indexOf(line.charAt(i)) == -1) {
                         System.err.println("Airport lookup malformed");
                         return null;
-                    // Checks for empty cells.
-                    // My braincells aren't passing this one.
                     } else if (line.charAt(i) == ',' && line.charAt(i + 1) == ',') {
                         System.err.println("Airport lookup malformed");
                         return null;
-                    // Crude fix for avoiding splitting at the darn coma in the middle of coordinates value.
                     } else if (line.charAt(i) == ',' && line.charAt(i + 1) == '"'){
                         skipComa = true;
-                        // You are going to see these 4 lines a lot more....
                         if (wordIndex > 5) {
                                 System.err.println("Airport lookup malformed");
                                 return null;
                         }
-                        // Aquires the value and the rest of the string using the helper function down bellow.
                         String[] temp = cutStrHere(tempLine, i-removedChar);
                         splitLine[wordIndex] = temp[0];
                         tempLine = temp[1];
                         removedChar += temp[0].length() + 1;
                         wordIndex++;
                         System.out.println("Cut with ,\" ");
-                    // This will run most of the time.
                     } else if (line.charAt(i) == ',') {
                         if (skipComa) {
                             skipComa = false;
@@ -119,9 +79,7 @@ public class FileIO {
                             wordIndex++;
                             System.out.println("Cut with ,");
                         }
-                    // In the end the string will be cut the point where the last value IS the string.
-                    // This took an impressive amount of time to figure out....
-                    } else if (i++ == line.length()) {
+                    } else if (i + 1 == line.length()) {
                         if (wordIndex > 5) {
                             System.err.println("Airport lookup malformed");
                             return null;
@@ -130,16 +88,9 @@ public class FileIO {
                         System.out.println("Added end");
                     }
                 }
-                // That's it huh....
                 csv.add(splitLine);
                 System.out.println("Done with line");
             }
-            // And here we create the Object to send back.
-            // You can probably see how the comments degrade the lower you go.
-            // That's because the Monster i drank earlier is loosing it's might over my brain stem.
-            // And also probably because I've been listening to the same Cyberpunk 2077 end credits song 
-            // for the past 30 minutes and Arasaka has long taken my soul...
-            // If you are older folk who doesn't play video games....I am so sorry for making you read all this nonsense....
             AirportLookup out = new AirportLookup(map, csv);
             return out;
         } catch (IOException e) {
@@ -147,17 +98,58 @@ public class FileIO {
             return null;
         }
     }
+    
+    // Reads airport lookup csv file into memory.
+    // AirportLookup object is used to return multiple values of different types.
+    public static AirportLookup readCsv(String path) {
+        String LEGAL_CHARACTERS = "QWERTYUIOPASDFGHJKLZXCVBNM" +
+                                    "qwertyuiopasdfghjklzxcvbnm" +
+                                    "1234567890" +
+                                    "!@#$%^&*()-_=+\\|[]{};':\",.<>/?`~ ";
+        try {
+            List<String[]> csv = new ArrayList<>();
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            if (lines.isEmpty()) {
+                System.err.println("Airport lookup malformed");
+                return null;
+            }
+            Map<String, Integer> map = getCSVColumnOrderMap(lines.getFirst().split(","));
+            if (map == null) {
+                System.err.println("Airport lookup malformed");
+                return null;
+            }
+            lines.removeFirst();
+            for (String line : lines) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (LEGAL_CHARACTERS.indexOf(line.charAt(i)) == -1) {
+                        System.err.println("Airport lookup malformed");
+                        return null;
+                    }
+                    if (line.charAt(i) == ',' && line.charAt(i+1) == ',') {
+                        System.err.println("Airport lookup malformed");
+                        return null;
+                    }
+                }
+                // Magic regex string that i barely understand.
+                // This is the only part of the project generated by an LLM.
+                // The previous iteration of this function was triple the size of this one,
+                // and this one regex string replaces most of the logic in the previous one.
+                // If you want to see the old function, go look up older commits.
+                csv.add(line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"));
+            }
+            return new AirportLookup(map, csv);
+        } catch (IOException e) {
+            System.err.println("Airport lookup not found");
+            return null;
+        }
+    }
 
-    // This is totally not stolen from StackOverflow...
-    // Returns the substring before and after the split point.
+    // REMOVE BEFORE PUSHING INTO REVIEW PHASE!!!
     private static String[] cutStrHere(String str, int i) {
         return new String[] {str.substring(0, i), str.substring(i + 1)};
     }
 
-    // Returns the map for the aiport lookup file.
-    // This is to handle such cases where the lookup file has different order for colomns.
-    // Also doubles as a filter for lookup files written by the illiterate.
-    // That including me since I can't seem to remember to capitalize my darn i's.
+
     public static Map<String, Integer> getCSVColumnOrderMap(String[] keys) {
         Map<String, Integer> OrderMap = new HashMap<>();
         if (Arrays.asList(keys).contains("name") &&
